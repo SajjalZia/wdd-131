41f83e83b73d24d04be6d647ab28ecf686c6b02f
Zia <sajjalzia.inbox@gmail.com>
2024-11-23 00:05:12 +0500

    wdd 131 homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Display the current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display the last modified date dynamically
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
